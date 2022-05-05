034213452e85f6e66cacae39e28d310925aa520f
Zesan <gmzesan7767@gmail.com>
2022-05-05 21:22:11 +0600

    token verify successful

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ app.use(cors());
 app.use(express.json());
 
 
-
+//verify token
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.288lx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -45,12 +59,17 @@ async function run() {
         });
 
         //my personal data
-        app.get("/myproducts", async (req, res) => {
-            const query = { email };
-            const cursor = organicCollection.find(query);
-            const myproduct = await cursor.toArray();
-            res.send(myproduct);
-            
+        app.get("/myproducts", verifyToken, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email };
+                const cursor = organicCollection.find(query);
+                const myproduct = await cursor.toArray();
+                res.send(myproduct);
+            } else {
+                res.status(403).send({ message: "Forbidden access" });
+            }
         });
 
         //Load a single data for a perticular id
